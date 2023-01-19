c3446f8a9217be4f7f907ec375f738084957ce05
Insoapa <122748724+Insoapa@users.noreply.github.com>
2023-01-19 03:51:01 -0500

    Update Cambridge EN-ES Dictionary1.js

--- Cambridge EN-ES Dictionary1.js
+++ Cambridge EN-ES Dictionary1.js
@@ -115,6 +115,7 @@ class enes_Cambridge {
             }
             let css = this.renderCSS();
             notes.push({
+                css,
                 expression,
                 reading,
                 definitions,
